ffb2fb80b51ca84347d2e4c3971ead7f96726b8a
hghardy2 <89137410+hghardy2@users.noreply.github.com>
2024-01-27 12:21:15 +0100

    HARDY - BOT

--- plugins/hdr.js
+++ plugins/hdr.js
@@ -1,134 +1,46 @@
 import FormData from "form-data";
 import Jimp from "jimp";
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-	switch (command) {
-		case "dehaze":
-			{
-				conn.enhancer = conn.enhancer ? conn.enhancer : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `photo?`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} tidak support`;
-				else conn.enhancer[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "dehaze");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("Proses Gagal :(");
-					}
-					delete conn.enhancer[m.sender];
-				}
-			}
-			break;
-		case "recolor":
-			{
-				conn.recolor = conn.recolor ? conn.recolor : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `photo?`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} tidak support`;
-				else conn.recolor[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "recolor");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("Proses Gagal :(");
-					}
-					delete conn.recolor[m.chat];
-				}
-			}
-			break;
-		case "hdr":
-			{
-				conn.hdr = conn.hdr ? conn.hdr : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `photo?`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} tidak support`;
-				else conn.hdr[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "enhance");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("Proses Gagal :(");
-					}
-					delete conn.hdr[m.sender];
-				}
-			}
-			break;
-	}
+const handler = async (m, {conn, usedPrefix, command}) => {
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) throw `*[❗] Reply to the picture using this command ${usedPrefix + command}*`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `*[❗] 𝙴𝙻 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙳𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 (${mime}) 𝙽𝙾 𝙴𝚂 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙱𝙻𝙴, 𝙴𝙽𝚅𝙸𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾*`;
+  m.reply("*[❗] Processing image...*");
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
+ } catch {
+  throw "*[❗] Error. try again later*";
+ }
 };
-handler.help = ["dehaze","recolor","hdr"];
-handler.tags = ["tools"];
-handler.command = ["dehaze","recolor","hdr"];
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.command = ["remini", "hd", "enhance"];
 export default handler;
 
-async function processing(urlPath, method) {
-	return new Promise(async (resolve, reject) => {
-		let Methods = ["enhance", "recolor", "dehaze"];
-		Methods.includes(method) ? (method = method) : (method = Methods[0]);
-		let buffer,
-			Form = new FormData(),
-			scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
-		Form.append("model_version", 1, {
-			"Content-Transfer-Encoding": "binary",
-			contentType: "multipart/form-data; charset=uttf-8",
-		});
-		Form.append("image", Buffer.from(urlPath), {
-			filename: "enhance_image_body.jpg",
-			contentType: "image/jpeg",
-		});
-		Form.submit(
-			{
-				url: scheme,
-				host: "inferenceengine" + ".vyro" + ".ai",
-				path: "/" + method,
-				protocol: "https:",
-				headers: {
-					"User-Agent": "okhttp/4.9.3",
-					Connection: "Keep-Alive",
-					"Accept-Encoding": "gzip",
-				},
-			},
-			function (err, res) {
-				if (err) reject();
-				let data = [];
-				res
-					.on("data", function (chunk, resp) {
-						data.push(chunk);
-					})
-					.on("end", () => {
-						resolve(Buffer.concat(data));
-					});
-				res.on("error", (e) => {
-					reject();
-				});
-			}
-		);
-	});
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
+    }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
 }
