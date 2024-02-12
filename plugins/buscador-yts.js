894d3604423edb00a466caf2af4c5f94ecb01358
hghardy2 <89137410+hghardy2@users.noreply.github.com>
2024-02-12 22:03:16 +0100

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '⚠️ What are you searching about ?';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
@@ -10,9 +10,9 @@ const handler = async (m, {conn, text}) => {
       case 'video': return `
 ° *_${v.title}_*
 ↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
+↳ 🕒 *_Time :_* ${v.timestamp}
+↳ 📥 *_Release :_* ${v.ago}
+↳ 👁 *_Views :_* ${v.views}`;
     }
   }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
